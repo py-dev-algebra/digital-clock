8c0ecb3fd234e72fbef0a43a0fc5ccd1de6aeba8
Krunoslav Lujic <python.developer.edukacija@gmail.com>
2024-01-23 21:10:49 +0100

    Finished

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
-from datetime import datetime as dt
-from dateutil import tz
 import time
 import os
-import locale
+# import py_digital_clock as pdc
+from py_digital_clock import digital_clock
 
 
-def digital_clock(custom_locale: str = 'hr_HR', 
-                  time_zone:str = 'Europe/Zagreb') -> str:
-    """Function returns curent time in predefined locale and time zone
-
-    Args:
-        custom_locale (str, optional): Locale in which will be displayed the name of day in week. Defaults to 'hr_HR'.
-        time_zone (str, optional): time in which will be displayed time. Defaults to 'Europe/Zagreb'.
-
-    Returns:
-        str: Returns constructed string from datetime object in format: week_day, day.month.year hours:minutes:seconds
-    """
-    locale.setlocale(locale.LC_TIME, locale=custom_locale)
-    custom_tz = tz.gettz(time_zone)
-    current_datetime = dt.now().astimezone(custom_tz)
-    return f'{current_datetime.strftime("%A").capitalize()}, {current_datetime.strftime("%d.%m.%Y. %H:%M:%S")}'
+def main():
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(digital_clock())
+        print(digital_clock('de_DE'))
+        print(digital_clock('fr_FR'))
+        time.sleep(1)
 
 
-
-while True:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print(digital_clock(),'\t',digital_clock('de_DE'))
-    time.sleep(1)
+if __name__ == '__main__':
+    main()
---
+++ py_digital_clock.py
@@ -0,0 +1,20 @@
+from datetime import datetime as dt
+from dateutil import tz
+import locale
+
+
+def digital_clock(custom_locale: str = 'hr_HR', 
+                  time_zone:str = 'Europe/Zagreb') -> str:
+    """Function returns curent time in predefined locale and time zone
+
+    Args:
+        custom_locale (str, optional): Locale in which will be displayed the name of day in week. Defaults to 'hr_HR'.
+        time_zone (str, optional): time in which will be displayed time. Defaults to 'Europe/Zagreb'.
+
+    Returns:
+        str: Returns constructed string from datetime object in format: week_day, day.month.year hours:minutes:seconds
+    """
+    locale.setlocale(locale.LC_TIME, locale=custom_locale)
+    custom_tz = tz.gettz(time_zone)
+    current_datetime = dt.now().astimezone(custom_tz)
+    return f'{current_datetime.strftime("%A").capitalize()}, {current_datetime.strftime("%d.%m.%Y. %H:%M:%S")}'
